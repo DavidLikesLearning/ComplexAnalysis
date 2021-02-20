0abf78773910016b0d477af9f4233adf34b0a5d7
David Jose Florez <sirdavid@stanford.edu>
2021-02-20 18:13:47 -0500

    more comments

--- trial.py
+++ trial.py
@@ -10,16 +10,19 @@ plt.plot(x,2*y)
 #one dimensional outputs (velocity of divergence or binary value
 #denoting convergence)
 bnds = [-6.0, 6.0, -6.0, 6.0]
-detail =.05
-xs = np.arange(bnds[0],bnds[1],detail)
+detail =.05 #could call it delta too
+xs = np.arange(bnds[0],bnds[1],detail) #arrays of x and y values to use
 ys = np.arange(bnds[2],bnds[3],detail)
 pixWidth =len(xs)
 out = np.zeros((pixWidth,pixWidth))
 for xi in range(pixWidth):
     for yi in range(pixWidth):
         if abs((xs[xi])**2 - (ys[yi])**2 -4)<.3:
-            out[xi,yi]= 1
+            out[xi,yi]= 1 # we'll just draw all points more or less near a hyperbola
 fig =plt.subplot(1,2,2)
 plt.imshow(out, extent=bnds )
+#this extent command changes the axes from the
+#indices of the canvas to axes labels defined from the bounds passed in
+
 plt.show()
 
