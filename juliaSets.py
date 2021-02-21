7847d390e11909cd4414627de5df87c498234f31
David Jose Florez <sirdavid@stanford.edu>
2021-02-20 21:42:43 -0500

    it works!

--- juliaSets.py
+++ juliaSets.py
@@ -1,23 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 #https://www.101computing.net/number-only/
 #^^helped get user input
-bnds = [-6.0,6.0,-6.0,6.0]
-delta = .05
+bnds = [-1.5,1.5,-1.5,1.5]
+delta = .00176
 xs = np.arange(bnds[0],bnds[1],delta)
 ys = np.arange(bnds[2],bnds[3],delta)
 size = len(xs)
 
-while True:
+while True: #getting the values
     try:
         rz = float(input("We need a complex number z. What should be Re(z)? "))
         iz = float(input("What should be Im(z)? "))
+        N = int(input("To what age do turtles live? An integer is best. "))
     except ValueError:
-        print("Not an float! Try again.")
+        print("Not a good number. Try again.")
         continue
     else:
         break
 
+z = complex(rz,iz)
+R = .5 + np.sqrt(.25 + abs(z)) # just a definition my youtube course suggested
+def f(x):
+    return(x**2 + z)
+out =np.zeros((size,size))
+for xi in range(size):
+    for yi in range(size):
+        num = complex(xs[xi], ys[yi])
+        itr =0
+        while abs(num) < R:
+            num = f(num)
+            itr = itr+1
+        out[xi,yi] = itr/N
 
-print(rz+iz)
+plt.subplot()
+plt.imshow(out, extent=bnds, cmap=cm.hot)
+
+plt.show()
+
